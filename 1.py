8ef8d3cbe1293ce1f1e493b804d86222ac49c0cc
Dm11tr <molodglup@yandex.ru>
2023-09-10 20:23:13 +0500

    second commit

--- 1.py
+++ 1.py
@@ -3,13 +3,13 @@
 # m — кол-во элементов второго множества. Затем пользователь вводит сами элементы множеств
 
 
-# from random import randint
-# n_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов первого множества: '))))
-# print(n_set)
-# m_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов второго множества: '))))
-# print(m_set)
-# s_set = sorted(n_set.intersection(m_set))
-# print(*s_set)
+from random import randint
+n_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов первого множества: '))))
+print(n_set)
+m_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов второго множества: '))))
+print(m_set)
+s_set = sorted(n_set.intersection(m_set))
+print(*s_set)
 
 
 # В фермерском хозяйстве в Карелии выращивают чернику. Она растёт на круглой грядке, причём кусты высажены только по окружности
